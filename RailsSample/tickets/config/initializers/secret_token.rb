dce6d1320700a6def93a893e3b69ff3b7e90a076
Hiroki Kumagai <hkkumagai@yahoo.co.jp>
2012-03-11 12:33:25 +0900

    rails sample add

---
+++ RailsSample/tickets/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tickets::Application.config.secret_token = '74f3701e7f4fd983e260f782ce93dad60d82838012b221b7e5a64ac28f26d6f9e9ba61e5855f58a507cc209424c19b976db7036647eb8edc86770a95aad7b67b'
